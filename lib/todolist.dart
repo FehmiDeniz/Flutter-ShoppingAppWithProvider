78912809d327a458dd0d2ddcbd48627f365d54f3
FehmiDeniz <fehmideniz06@gmail.com>
2022-10-20 14:57:51 +0300

    todolist

---
+++ lib/todolist.dart
@@ -0,0 +1,15 @@
+/*
+>consumer, provider video
+
+top deal kısmı için widget ve jsondan veri 
+3. sayfada  siparişler için sepete eklenenleri göster (belki widget)
+tüm sayfalarda provider 
+sayfa 2  ürün id'ye göre verileri görüntülesin
+ 
+
+
+
+
+
+
+ */
